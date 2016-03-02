1341d5d6d1cdc3a869a6b43eed49f2b2137c78e3
adamliu <adamliu@aftership.net>
2016-03-02 18:54:36 +0800

    update

--- webhook.php
+++ webhook.php
@@ -3,5 +3,5 @@ print_r($_POST);
 
 echo "going to write log...\n";
 file_put_contents("php://stderr", "&&&\n");
-file_put_contents("php://stderr", print_r($_POST));
+file_put_contents("php://stderr", $_POST);
 echo "end write the log...";
