08a979dca68638e76701c24d575448eb63e42cb7
adamliu <adamliu@aftership.net>
2016-03-02 18:43:29 +0800

    update

--- webhook.php
+++ webhook.php
@@ -1,2 +1,9 @@
 <?php
 print_r($_POST);
+
+echo "going to write log...";
+$myfile = fopen("log.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $_POST);
+fclose($myfile);
+echo "end write the log...";
