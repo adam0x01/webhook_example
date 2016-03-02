e03d4ac988b087eab40d3c1bc4fd4ffe742a6e7c
adamliu <adamliu@aftership.net>
2016-03-02 19:07:05 +0800

    update

--- webhook.php
+++ webhook.php
@@ -1,7 +1,13 @@
 <?php
+
+echo "post...\n";
 print_r($_POST);
+
+echo "\njson...\n";
+$inputJSON = file_get_contents('php://input');
+$input= json_decode( $inputJSON, TRUE ); //convert JSON into array
 
 echo "going to write log...\n";
 file_put_contents("php://stderr", "&&&\n");
-file_put_contents("php://stderr", $_POST);
+file_put_contents("php://stderr", $input);
 echo "end write the log...";
