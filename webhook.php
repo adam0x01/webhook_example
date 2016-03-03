53adad55e4719fe59272214d4e11b89666cf0b92
adamliu <adamliu@aftership.net>
2016-03-03 11:12:48 +0800

    update

--- webhook.php
+++ webhook.php
@@ -1,15 +1,17 @@
 <?php
 
 echo "post...\n";
-print_r($_POST);
 
+// Coz aftership webhook send post request with 'Content-Type: application/json' header, $_POST can't get the post body.
+// http://stackoverflow.com/questions/2731297/file-get-contentsphp-input-or-http-raw-post-data-which-one-is-better-to
+// try this method
 echo "\njson...\n";
-// $inputJSON = file_get_contents('php://input');
-// $input= json_decode( $inputJSON, TRUE ); //convert JSON into array
+$inputJSON = file_get_contents("php://input");
+$input= json_decode( $inputJSON, TRUE ); //convert JSON into array
 
-$entityBody = stream_get_contents(STDIN);
+// $entityBody = stream_get_contents(STDIN);
 
 echo "going to write log...\n";
 file_put_contents("php://stderr", "&&&\n");
-file_put_contents("php://stderr", $entityBody);
+file_put_contents("php://stderr", $input);
 echo "end write the log...";
