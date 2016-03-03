dbcab002ebf40ad0c151a6acfdb24c738b1be2d3
adamliu <adamliu@aftership.net>
2016-03-03 11:01:20 +0800

    update

--- webhook.php
+++ webhook.php
@@ -4,10 +4,12 @@ echo "post...\n";
 print_r($_POST);
 
 echo "\njson...\n";
-$inputJSON = file_get_contents('php://input');
-$input= json_decode( $inputJSON, TRUE ); //convert JSON into array
+// $inputJSON = file_get_contents('php://input');
+// $input= json_decode( $inputJSON, TRUE ); //convert JSON into array
+
+$entityBody = stream_get_contents(STDIN);
 
 echo "going to write log...\n";
 file_put_contents("php://stderr", "&&&\n");
-file_put_contents("php://stderr", $input);
+file_put_contents("php://stderr", $entityBody);
 echo "end write the log...";
